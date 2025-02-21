05218ed9117ef4f8483beb6e48b7c67018ee0c5e
divanov11 <ivanovsin11@gmail.com>
2025-02-21 11:38:34 -0800

    Updated env

--- src/lib/appwriteConfig.js
+++ src/lib/appwriteConfig.js
@@ -2,7 +2,7 @@ import { Client, Account, ID } from "appwrite";
 
 const client = new Client();
 
-client.setEndpoint(import.meta.env.VITE_APPWRITE_URL);
+client.setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT);
 client.setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 const account = new Account(client);
