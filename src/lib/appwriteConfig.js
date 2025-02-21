bc85688b0705a1dda9c715e759d137d6ad00a0a8
divanov11 <ivanovsin11@gmail.com>
2025-02-21 11:35:51 -0800

    Updated appwrite endpoint url

--- src/lib/appwriteConfig.js
+++ src/lib/appwriteConfig.js
@@ -2,8 +2,10 @@ import { Client, Account, ID } from "appwrite";
 
 const client = new Client();
 
-client.setEndpoint(import.meta.env.APPWRITE_ENDPOINT);
-client.setProject(import.meta.env.APPWRITE_PROJECT_ID);
+console.log(import.meta.env.VITE_APPWRITE_ENDPOINT);
+
+client.setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT);
+client.setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 const account = new Account(client);
 
